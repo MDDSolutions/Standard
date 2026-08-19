cb8dce3f4ad00214b0bf904f81e228098174112f
Michael DeFehr <mdefehr@defehr.com>
2026-08-19 18:55:11 -0400

    tests for the new RunSqlUpdate and AutoParam overloads

--- DBEngineUnitTests/SimpleTests.cs
+++ DBEngineUnitTests/SimpleTests.cs
@@ -36,5 +36,56 @@ namespace DBEngineUnitTests
             Assert.AreEqual(SqlDbType.Int, integerParameter.SqlDbType);
             Assert.AreEqual(SqlDbType.VarChar, textParameter.SqlDbType);
         }
+
+        [TestMethod]
+        public void ProcedureParameterPreservesMetadataForNullValues()
+        {
+            var metadata = new ProcedureParameter
+            {
+                name = "@Data",
+                type_name = "varbinary",
+                max_length = -1,
+                precision = 0,
+                scale = 0
+            };
+
+            var parameter = metadata.CreateSqlParameter(null);
+
+            Assert.AreEqual("@Data", parameter.ParameterName);
+            Assert.AreEqual(DBNull.Value, parameter.Value);
+            Assert.AreEqual(SqlDbType.VarBinary, parameter.SqlDbType);
+            Assert.AreEqual(-1, parameter.Size);
+        }
+
+        [TestMethod]
+        public void ProcedureParameterConvertsUnicodeByteLengthToCharacterLength()
+        {
+            var metadata = new ProcedureParameter
+            {
+                name = "@Text",
+                type_name = "nvarchar",
+                max_length = 200
+            };
+
+            var parameter = metadata.CreateSqlParameter("hello");
+
+            Assert.AreEqual(SqlDbType.NVarChar, parameter.SqlDbType);
+            Assert.AreEqual(100, parameter.Size);
+        }
+
+        [TestMethod]
+        public void ExpressionRunSqlUpdateOverloadHasAProcedureSpecificSignature()
+        {
+            var db = new DBEngine("server=.;database=tempdb;Trusted_Connection=true;", "DBEngineUnitTests");
+
+            Func<ParameterTarget, System.Linq.Expressions.Expression<Func<object>>, bool> call =
+                (target, expression) => db.RunSqlUpdate(target, "dbo.TestProcedure", -1, null, expression);
+
+            Assert.IsNotNull(call);
+        }
+
+        private class ParameterTarget
+        {
+        }
     }
 }
